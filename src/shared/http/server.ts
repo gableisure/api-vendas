43e37389d8d0ad3ecc2fabeb713092108c1c0fe8
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-07-20 14:01:48 -0300

    [feat]: configuração do typeorm e conexão com o banco de dados

---
+++ src/shared/http/server.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata'; 
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const PORT = 3333;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
+});
