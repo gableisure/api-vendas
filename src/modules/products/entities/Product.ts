1279676b044416e3e24a91217fbc3152d3c45cce
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-07-22 14:11:36 -0300

    [feat]: add entity products

---
+++ src/modules/products/entities/Product.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column('decimal')
+    price: number;
+
+    @Column('int')
+    quantity: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @CreateDateColumn()
+    update_at: Date;
+
+}
+
+export default Product;
