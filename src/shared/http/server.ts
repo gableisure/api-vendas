0d4eacbe51b8c4196f157993db4203c8b777a78d
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-08-26 13:19:56 -0300

    [feat]: add request data validation with celebrate lib

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata'; 
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { errors } from 'celebrate'
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -7,11 +9,13 @@ import '@shared/typeorm';
 
 const PORT = 3333;
 const app = express();
-
+ 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     if(error instanceof AppError){
